576e3421e2ac3bb6f8d7f3fd8de88b64e2f34293
Carlos Mejia <cdmejia4@utpl.edu.ec>
2023-05-31 17:27:14 -0500

    subida clase miercoles

--- Repaso/src/paquete3/LecturaSecuencialJugador.java
+++ Repaso/src/paquete3/LecturaSecuencialJugador.java
@@ -19,6 +19,7 @@ public class LecturaSecuencialJugador {
     private ObjectInputStream entrada;
     private ArrayList<Jugador> jugadores;
     private String nombreArchivo;
+    private Jugador registroBuscado;
 
     public LecturaSecuencialJugador(String n) {
         nombreArchivo = n;
@@ -73,6 +74,40 @@ public class LecturaSecuencialJugador {
     public String obtenerNombreArchivo() {
         return nombreArchivo;
     }
+    
+     public void establecerRegistroBuscado(String cadena) {
+        // 
+        
+        File f = new File(obtenerNombreArchivo());
+        if (f.exists()) {
+
+            while (true) {
+                try {
+                    Jugador registro = (Jugador) entrada.readObject();
+                    if (registro.obtenerNombre().equals(cadena)) {
+                        registroBuscado = registro;
+                    }
+                } catch (EOFException endOfFileException) {
+                    return; // se llegó al fin del archivo
+                    // se puede usar el break;
+                    // System.err.println("Fin de archivo: " + endOfFileException);
+
+                } catch (IOException ex) {
+                    System.err.println("Error al leer el archivo: " + ex);
+                } catch (ClassNotFoundException ex) {
+                    System.err.println("No se pudo crear el objeto: " + ex);
+                } catch (Exception ex) {
+                    System.err.println("No hay datos en el archivo: " + ex);
+
+                }
+            }
+        }
+    }
+
+    public Jugador obtenerRegistroBuscado() {
+        return registroBuscado;
+    }
+    
 
     @Override
     public String toString() {
